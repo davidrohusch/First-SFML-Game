57ac2c320cf3562a396a41efaa42c08696e0656d
David Rohusch <davidrohusch@localhost.localdomain>
2020-09-15 01:10:56 +0200

    second try

---
+++ cmake-build-debug/CMakeFiles/CPP_first.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_first.dir/main.cpp.o"
+  "CPP_first"
+  "CPP_first.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+
+#include <iostream>
+
+#define windowWidth 1024
+#define windowHeight 800
+
+using namespace std;
+
+
+class Player{
+public:
+
+    float x;
+    float y;
+
+    float ballR = 30.f;
+    float startX = windowWidth/2;
+    float startY = windowHeight-ballR*2;
+
+    float speed = 0.04;
+
+
+
+};
+
+
+int main()
+{
+    //sf::Window window;
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Jumper", sf::Style::Close);
+   // sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::CircleShape shape(30.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(windowWidth/2,windowHeight-60);
+    Player ball;
+    ball.x = ball.startX;
+    ball.y = ball.startY;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            ball.x+=ball.speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            ball.x-=ball.speed;
+        }
+
+        shape.setPosition(ball.x,ball.y);
+        window.clear();
+        window.draw(shape);
+
+        window.display();
+    }
+
+    return 0;
+}
+
